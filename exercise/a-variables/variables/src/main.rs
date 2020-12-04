f6d881481552c36d56beb4c3bbeccb050117cd08
Sushil Mayengbam <hawaijar@Sushils-MacBook-Pro.local>
2020-12-04 23:23:58 +0530

    Added answers for exercise:a-variables

---
+++ exercise/a-variables/variables/src/main.rs
@@ -0,0 +1,8 @@
+fn main() {
+    const STARTING_MISSILES: i32 = 8;
+    const READY_AMOUNT: i32 = 2;
+    println!(
+        "Firing {} of my {} missiles...",
+        STARTING_MISSILES, READY_AMOUNT
+    );
+}
